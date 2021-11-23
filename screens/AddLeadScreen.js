775e5437ad787560beec7ae4e76de3eb0dfdf9de
dkolobear <wonderdream.dev@gmail.com>
2021-11-23 04:01:12 +0100

    change fontfamily

---
+++ screens/AddLeadScreen.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { 
+  SafeAreaView, 
+  Text, 
+  View, 
+  ScrollView,
+  StyleSheet,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+
+export default function AddLeadScreen() {
+  return (
+    <View>
+
+    </View>
+  )
+}
---
+++ screens/LocationInfoScreen.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { 
+  SafeAreaView, 
+  Text, 
+  View, 
+  ScrollView,
+  StyleSheet,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
+
+import FilterButton from '../components/FilterButton';
+import Divider from '../components/Divider';
+import { PRIMARY_COLOR } from '../constants/Colors';
+
+export default function LocationInfo() {
+  return (
+    <View>
+      
+    </View>
+  )
+}
