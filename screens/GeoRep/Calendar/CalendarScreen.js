80e10cad92656d338f2e03171375adb6c5fca8a4
Vinod Kumar <vinod@ckinformatics.org>
2022-02-17 17:54:21 +0530

    GVARN-94

--- screens/GeoRep/Calendar/CalendarScreen.js
+++ screens/GeoRep/Calendar/CalendarScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, Text, View, StyleSheet, TouchableOpacity , FlatList } from 'react-native';
+import { SafeAreaView, Text, View, StyleSheet, TouchableOpacity, FlatList, SectionList } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faAngleDoubleRight, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import SvgIcon from '../../../components/SvgIcon';
@@ -9,11 +9,12 @@ import { BG_COLOR } from '../../../constants/Colors';
 import Fonts from '../../../constants/Fonts';
 import { checkFeatureIncludeParam, getBaseUrl, getToken } from '../../../constants/Storage';
 import { getCalendar, updateCalendar } from '../../../actions/calendar.action';
-import { useSelector, useDispatch , connect} from 'react-redux';
+import { useSelector, useDispatch, connect } from 'react-redux';
 import { CalendarItem } from './partial/CalendarItem';
-import DraggableFlatList , {ScaleDecorator , useOnCellActiveAnimation}  from 'react-native-draggable-flatlist'
+import DraggableFlatList, { ScaleDecorator, useOnCellActiveAnimation } from 'react-native-draggable-flatlist'
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { LOCATION_LOOP_LISTS } from '../../../actions/actionTypes';
+import moment from 'moment';
 var selectedIndex = 0;
 
 export default function CalendarScreen(props) {
@@ -23,82 +24,128 @@ export default function CalendarScreen(props) {
   const currentLocation = useSelector(state => state.rep.currentLocation);
   const [tabIndex, setTabIndex] = useState(2);
   const [lists, setLists] = useState([]);
+  const [todayList, setTodayList] = useState([]);
   const [isOptimize, setIsOptimize] = useState(false);
   const [isAdd, setIsAdd] = useState(false);
-  
-  useEffect(() => {        
+
+  useEffect(() => {
     if (props.screenProps) {
       props.screenProps.setOptions({
         title: "Calendar"
       });
-    }    
+    }
   }, []);
 
   useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {                
-      if(selectedIndex === 1){
+    const unsubscribe = navigation.addListener('focus', () => {
+      if (selectedIndex === 1) {
         loadList("last_week");
-      }else if(selectedIndex === 2 || selectedIndex === 0){
+      } else if (selectedIndex === 2 || selectedIndex === 0) {
         loadList("today");
-      }else if(selectedIndex === 3){
+      } else if (selectedIndex === 3) {
         loadList("week_ahead");
       }
     });
     return unsubscribe;
   }, [navigation]);
 
-  const loadList = async(type) => {    
-    setIsOptimize( await checkFeatureIncludeParam("calendar_optimize"));
+  const loadList = async (type) => {
+    setIsOptimize(await checkFeatureIncludeParam("calendar_optimize"));
     setIsAdd(await checkFeatureIncludeParam("calendar_add"));
     var base_url = await getBaseUrl();
     var token = await getToken();
-    if(base_url != null && token != null){      
+    if (base_url != null && token != null) {
       getCalendar(base_url, token, type)
-      .then(res => {        
-        setLists(res);                    
-      })
-      .catch(error=>{
-        setLists([]);
-      });
-    }    
+        .then(res => {
+          console.log(res);
+          if (selectedIndex == 2) {
+            setTodayList(res);
+          } else {
+            updateListForWeek(res);
+          }
+        })
+        .catch(error => {
+          setLists([]);
+          setTodayList([]);
+        });
+    }
   }
 
-  const updateTodayLocationLists =  async(data) => {    
-      updateCalendar({schedules:data})
-      .then(res => {                          
-      })
-      .catch(error=>{        
-      });        
+  const updateListForWeek = (res) => {
+    let data = [];
+    console.log("week 1");
+    for (let i = 0; i < res.length; i++) {
+      if (data.length == 0) {
+        let record = [];
+        record.push(res[i]);
+        data.push({
+          'title': res[i].schedule_date,
+          'data': record
+        })
+
+      } else {
+        let scheduleExist = false;
+        for (let j = 0; j < data.length; j++) {
+          let recordExist = false;
+          if (data[j].title === res[i].schedule_date) {
+            for (let k = 0; k < data[j].data.length; k++) {
+              if (data[j].data[k].schedule_id === res[i].schedule_id) {
+                recordExist = true;
+              }
+            }
+            if (!recordExist) {
+              data[j].data.push(res[i]);
+            }
+            scheduleExist = true;
+          }
+        }
+        if (!scheduleExist) {
+          data.push({
+            title: res[i].schedule_date,
+            data: [res[i]]
+          })
+        }
+      }
+    }
+    setLists(data);
   }
-    
-  const renderCalendarItem = (item, index , tabIndex) => {    
+
+  const updateTodayLocationLists = async (data) => {
+    updateCalendar({ schedules: data })
+      .then(res => {
+      })
+      .catch(error => {
+      });
+  }
+
+  const renderCalendarItem = (item, index, tabIndex) => {
     console.log("tabIndex", tabIndex);
-      return (
-      <View style={{marginTop:10}}>
-        <CalendarItem key={index} navigation={props.navigation} item={item} current={currentLocation} tabIndex={tabIndex} onItemSelected={() => {}}>
+    return (
+      <View style={{ marginTop: 10 }}>
+        <CalendarItem key={index} navigation={props.navigation} item={item} current={currentLocation} tabIndex={tabIndex} onItemSelected={() => { }}>
         </CalendarItem>
       </View>)
   }
-  
-  const renderTodayItem = ({item, drag} )  => {    
+
+  const renderTodayItem = ({ item, drag }) => {
     const { isActive } = useOnCellActiveAnimation();
     return (
       <ScaleDecorator>
-        <TouchableOpacity          
+        <TouchableOpacity
           onLongPress={drag}
           disabled={isActive}
           style={[
-            isActive ? {} : { marginTop:10},
+            isActive ? {} : { marginTop: 10 },
             { backgroundColor: isActive ? "#eee" : BG_COLOR },
           ]}>
 
-          <CalendarItem 
-          onItemSelected={() => {            
-            console.log("loop list updated in calendar ");
-            
-            dispatch({type: LOCATION_LOOP_LISTS, payload: lists });            
-          }}
-          key={item.schedule_id} navigation={props.navigation} item={item} current={currentLocation} tabIndex={tabIndex} > </CalendarItem>
+          <CalendarItem
+            onItemSelected={() => {
+              console.log("loop list updated in calendar ");
+
+              dispatch({ type: LOCATION_LOOP_LISTS, payload: lists });
+            }}
+            key={item.schedule_id} navigation={props.navigation} item={item} current={currentLocation} tabIndex={tabIndex} > </CalendarItem>
 
         </TouchableOpacity>
       </ScaleDecorator>
@@ -115,7 +162,7 @@ export default function CalendarScreen(props) {
             selectedIndex = 1;
             loadList("last_week");
           }}>
-          <Text style={[styles.tabText, tabIndex === 1 ? styles.tabActiveText : {}]}>Last Week</Text>
+            <Text style={[styles.tabText, tabIndex === 1 ? styles.tabActiveText : {}]}>Last Week</Text>
           </TouchableOpacity>
 
           <TouchableOpacity style={styles.tabItem} onPress={() => {
@@ -129,75 +176,78 @@ export default function CalendarScreen(props) {
           <TouchableOpacity style={styles.tabItem} onPress={() => {
             setTabIndex(3);
             selectedIndex = 3;
-            loadList("week_ahead");            
-            }}>
-          <Text style={[styles.tabText, tabIndex === 3 ? styles.tabActiveText : {}]}>Week Ahead</Text>
+            loadList("week_ahead");
+          }}>
+            <Text style={[styles.tabText, tabIndex === 3 ? styles.tabActiveText : {}]}>Week Ahead</Text>
           </TouchableOpacity>
         </View>
 
         {
-          false && 
+          false &&
           <TouchableOpacity style={styles.startButton} onPress={() => console.log("pressed")}>
             <Text style={[styles.startButtonText]}>Start My Day</Text>
-            <FontAwesomeIcon style={styles.startButtonIcon} size={25} color="#fff" icon={ faAngleDoubleRight } />
+            <FontAwesomeIcon style={styles.startButtonIcon} size={25} color="#fff" icon={faAngleDoubleRight} />
           </TouchableOpacity>
         }
 
-        <View style={{flex:1}}>
+        <View style={{ flex: 1 }}>
           {
             (tabIndex == 1 || tabIndex == 3) &&
-            <FlatList
-              data={lists}
-              renderItem={
-                  ({ item , index }) => renderCalendarItem(item, index, tabIndex)
-              }
+            <SectionList
               keyExtractor={(item, index) => index.toString()}
-              contentContainerStyle={{ paddingHorizontal: 7, marginTop: 15 }}
+              sections={lists}
+              renderItem={({ item, index }) => {
+                return renderCalendarItem(item, index, tabIndex)
+              }}
+              renderSectionHeader={({ section }) => {
+                console.log(section);
+                return <Text style={[styles.itemTitle, { textDecorationLine: 'underline', fontWeight: '700' }]}>{moment(section.title).format('dddd DD MMM YYYY')}</Text>
+              }}
             />
           }
           {
-            tabIndex == 2 &&   
+            tabIndex == 2 &&
             <GestureHandlerRootView>
               <DraggableFlatList
-                data={lists}
+                data={todayList}
                 onDragEnd={({ data }) => {
                   var tmp = [];
                   var newlists = [];
-                  data.forEach((item , index) =>{
+                  data.forEach((item, index) => {
                     item.schedule_order = (index + 1).toString();
                     newlists.push(item);
-                    tmp.push({schedule_order:(index + 1).toString() , location_id: item.location_id, schedule_id: item.schedule_id, schedule_date: item.schedule_date, schedule_time: item.schedule_time });
+                    tmp.push({ schedule_order: (index + 1).toString(), location_id: item.location_id, schedule_id: item.schedule_id, schedule_date: item.schedule_date, schedule_time: item.schedule_time });
                   });
-                  setLists(newlists);             
+                  setTodayList(newlists);
                   updateTodayLocationLists(tmp);
-                }}   
+                }}
                 keyExtractor={(item) => item.schedule_id}
-                renderItem={renderTodayItem}                
+                renderItem={renderTodayItem}
               />
-            </GestureHandlerRootView>            
-          }          
-        </View>                          
+            </GestureHandlerRootView>
+          }
+        </View>
       </View>
-        
+
       <View style={styles.plusButtonContainer}>
         {
-          isOptimize && 
-          <TouchableOpacity style={style.innerPlusButton} onPress={() =>{
-            props.navigation.navigate('CRM', {'screen': 'LocationSearch' , params : {'calendar_type': 'add'} });
+          isOptimize &&
+          <TouchableOpacity style={style.innerPlusButton} onPress={() => {
+            props.navigation.navigate('CRM', { 'screen': 'LocationSearch', params: { 'calendar_type': 'add' } });
           }}>
             <SvgIcon icon="Calendar_Optimize" width='70px' height='70px' />
           </TouchableOpacity>
-        }        
+        }
 
         {
           isAdd &&
           <TouchableOpacity style={style.innerPlusButton} onPress={() => {
-            props.navigation.navigate('CRM', {'screen': 'LocationSearch' , params : {'calendar_type': 'optimize'} });
+            props.navigation.navigate('CRM', { 'screen': 'LocationSearch', params: { 'calendar_type': 'optimize' } });
           }}>
             <SvgIcon icon="Round_Btn_Default_Dark" width='70px' height='70px' />
-          </TouchableOpacity>        
-        }        
-      </View>      
+          </TouchableOpacity>
+        }
+      </View>
     </SafeAreaView>
   )
 }
@@ -250,14 +300,19 @@ const styles = StyleSheet.create({
   startButtonIcon: {
     position: 'absolute',
     right: 10
-  },    
+  },
   plusButtonContainer: {
     position: 'absolute',
-    flexDirection:"row",
+    flexDirection: "row",
     bottom: 20,
     right: 20,
     zIndex: 1,
     elevation: 1,
+  },
+  itemTitle: {
+    fontSize: 14,
+    fontFamily: Fonts.secondaryMedium,
+    color: PRIMARY_COLOR
   },
 
 
