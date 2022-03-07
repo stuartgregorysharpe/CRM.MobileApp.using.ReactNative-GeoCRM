2ee08394a968b6ca1416029e63ee63d571a8eda8
Stuart <codechief19900@gmail.com>
2022-03-07 17:04:23 +0800

    GVARN-114 & Forms Lists

--- constants/Colors.js
+++ constants/Colors.js
@@ -8,32 +8,65 @@ export const DISABLED_COLOR = "#9D9FA2";
 export const TICK_BOX_COLOR = "#002a55";
 
 export const whiteLabel = () => {    
-      return {
-        headerBackground: '#133C8B',
-        headerText: '#FFFFFF',
-        inactiveIcon: '#B8B8B8',
-        activeIcon: '#133C8B',
-        actionFullButtonBackground: '133C8B',
-        actionFullButtonText: '#FFFFFF',
-        helpText: '#9D9FA2',
-        mainText: '#133C8B',
-        inputText: '#23282D',
-        actionOutlineButtonText: '#133C8B',
-        fieldBorder: '#133C8B',
-        itemSelectedBakground: '#3D8FFB',
-        itemSelectedIconFill: '#3D8FFB',
-        itemSelectedText: '#FFFFFF',
-        subText: '#9D9FA2',
-        inactiveTabText: '#9D9FA2',
-        activeTabText: '#133C8B',
-        activeTabUnderline: '#133C8B',
-        startDayBackground: '#9D9FA2',
-        startDayText: '#FFFFFF',
-        endDayBackground: '#DC143C',
-        endDayText: '#FFFFFF',
-        countBoxBackground: '#133C8B',
-        countBoxText: '#FFFFFF'
-      };    
+  var theme = "theme1";
+  if(theme === "theme1"){
+    return {
+      headerBackground: '#133C8B',
+      headerText: '#FFFFFF',
+      inactiveIcon: '#B8B8B8',
+      activeIcon: '#133C8B',
+      actionFullButtonBackground: '133C8B',
+      actionFullButtonText: '#FFFFFF',
+      helpText: '#9D9FA2',
+      mainText: '#133C8B',
+      inputText: '#23282D',
+      actionOutlineButtonText: '#133C8B',
+      fieldBorder: '#133C8B',
+      itemSelectedBakground: '#3D8FFB',
+      itemSelectedIconFill: '#3D8FFB',
+      itemSelectedText: '#FFFFFF',
+      subText: '#9D9FA2',
+      inactiveTabText: '#9D9FA2',
+      activeTabText: '#133C8B',
+      activeTabUnderline: '#133C8B',
+      startDayBackground: '#9D9FA2',
+      startDayText: '#FFFFFF',
+      endDayBackground: '#DC143C',
+      endDayText: '#FFFFFF',
+      countBoxBackground: '#133C8B',
+      countBoxText: '#FFFFFF'
+    }; 
+  }
+
+  if(theme === "theme2"){
+    return {
+      headerBackground: '#133C8B',
+      headerText: '#FFFFFF',
+      inactiveIcon: '#B8B8B8',
+      activeIcon: '#133C8B',
+      actionFullButtonBackground: '133C8B',
+      actionFullButtonText: '#FFFFFF',
+      helpText: '#9D9FA2',
+      mainText: '#133C8B',
+      inputText: '#23282D',
+      actionOutlineButtonText: '#133C8B',
+      fieldBorder: '#133C8B',
+      itemSelectedBakground: '#3D8FFB',
+      itemSelectedIconFill: '#3D8FFB',
+      itemSelectedText: '#FFFFFF',
+      subText: '#9D9FA2',
+      inactiveTabText: '#9D9FA2',
+      activeTabText: '#133C8B',
+      activeTabUnderline: '#133C8B',
+      startDayBackground: '#9D9FA2',
+      startDayText: '#FFFFFF',
+      endDayBackground: '#DC143C',
+      endDayText: '#FFFFFF',
+      countBoxBackground: '#133C8B',
+      countBoxText: '#FFFFFF'
+    }; 
+  }
+         
 };
 
 export default {
