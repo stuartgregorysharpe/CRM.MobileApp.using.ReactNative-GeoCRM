4e39dc23c2c283ff0cfec5c8416a24a8fd77da65
Dev <redflame1028@gmail.com>
2022-07-14 22:15:24 +0800

    GVARN-209: bugfix on self share and local search

--- screens/GeoRep/Touchpoint/containers/LeaderboardContainer.js
+++ screens/GeoRep/Touchpoint/containers/LeaderboardContainer.js
@@ -7,32 +7,45 @@ import {getLeaderboardItems} from '../helper';
 
 const LeaderboardContainer = props => {
   const [keyword, setKeyword] = useState('');
+  const [allItems, setAllItems] = useState([]);
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     onLoad();
+  }, []);
+  useEffect(() => {
+    onLocalFilter();
   }, [keyword]);
   const onSearch = text => {
     setKeyword(text);
   };
   const onFilterPress = () => {};
   const onItemAction = ({type, item}) => {};
+
   const onLoad = () => {
-    const params = {
-      username: keyword,
-    };
+    const params = {};
     setIsLoading(true);
     getApiRequest('touchpoints/leaderboard', params)
       .then(data => {
         const fetchedItems = getLeaderboardItems(data);
         setIsLoading(false);
         setItems(fetchedItems);
+        setAllItems(fetchedItems);
       })
       .catch(error => {
         setIsLoading(false);
       });
   };
-
+  const onLocalFilter = () => {
+    if (!keyword || keyword == '') {
+      setItems(allItems);
+      return;
+    }
+    const filteredItems = allItems.filter(x => {
+      return x.username.includes(keyword);
+    });
+    setItems(filteredItems);
+  };
   return (
     <View style={[styles.container, props.style]}>
       <SearchBar
