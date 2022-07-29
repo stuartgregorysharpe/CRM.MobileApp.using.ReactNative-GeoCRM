b1abe627a49d53dc730102a085e4b9dbdd6abea4
Dev <redflame1028@gmail.com>
2022-07-29 23:56:02 +0800

    use gms only for ios

--- services/AvailabilityService/index.js
+++ services/AvailabilityService/index.js
@@ -1,7 +1,9 @@
 import {gmsCheckAvailability} from './GmsAvailabilityService';
 import {hmsCheckAvailability} from './HmsAvailabilityService';
+import {Platform} from 'react-native';
 
 export async function isHMSService() {
+  if (Platform.OS == 'ios') return false;
   const isGmsAvailable = await gmsCheckAvailability();
   const isHmsAvailable = await hmsCheckAvailability();
   //return isHmsAvailable;
