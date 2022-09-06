6972c20e45009df2125cdca58a5a25cb034835b5
Dev <redflame1028@gmail.com>
2022-09-06 23:12:21 +0800

    GVARN-243: accept all button

--- screens/GeoRep/Stock/staging/StagingView.js
+++ screens/GeoRep/Stock/staging/StagingView.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useRef} from 'react';
 import {View, StyleSheet} from 'react-native';
 import QRScanModal from '../../../../components/common/QRScanModal';
 import SearchBar from '../../../../components/SearchBar';
+import {SubmitButton} from '../../../../components/shared/SubmitButton';
 import ShipmentScanResultView from './components/ShipmentScanResultView';
 import StagingShipmentList from './components/StagingShipmentList';
 
@@ -39,7 +40,16 @@ const StagingView = props => {
           );
         }}
       />
-      <StagingShipmentList items={shipments} />
+      <StagingShipmentList items={shipments} style={{flex: 1}} />
+      <SubmitButton
+        title={'Accept All'}
+        onSubmit={() => {
+          if (props.onPress) {
+            props.onPress();
+          }
+        }}
+        style={styles.submitButton}
+      />
     </View>
   );
 };
@@ -47,6 +57,11 @@ const StagingView = props => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  submitButton: {
+    alignSelf: 'stretch',
+    marginHorizontal: 10,
+    marginBottom: 16,
   },
 });
 
