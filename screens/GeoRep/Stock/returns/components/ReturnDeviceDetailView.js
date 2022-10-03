4f0dce655402709b3f86e312d78db302213bb7ab
Dev <redflame1028@gmail.com>
2022-10-03 14:45:31 +0800

    Add Device Cleanup on reason list in return device

--- screens/GeoRep/Stock/returns/components/ReturnDeviceDetailView.js
+++ screens/GeoRep/Stock/returns/components/ReturnDeviceDetailView.js
@@ -1,77 +1,88 @@
-import { StyleSheet, Dimensions, ScrollView } from 'react-native'
-import React , { useState } from 'react'
+import {StyleSheet, Dimensions, ScrollView} from 'react-native';
+import React, {useState} from 'react';
 import CSingleSelectInput from '../../../../../components/common/SelectInput/CSingleSelectInput';
-import { AppText } from '../../../../../components/common/AppText';
-import { whiteLabel } from '../../../../../constants/Colors';
-import { SubmitButton } from '../../../../../components/shared/SubmitButton';
+import {AppText} from '../../../../../components/common/AppText';
+import {whiteLabel} from '../../../../../constants/Colors';
+import {SubmitButton} from '../../../../../components/shared/SubmitButton';
 import DropdownInput from '../../../../../components/common/DropdownInput/DropdownInput';
 import TakePhotoView from '../../../../../components/shared/TakePhotoView';
-import { Strings } from '../../../../../constants';
+import {Strings} from '../../../../../constants';
 
 export default function ReturnDeviceDetailView(props) {
-  
-  const {lists , onReturnDevice, onPhotos, onReturnStock} = props;    
-  const [reason, setReason] = useState("");
-  const [reasonLists , setReasonLists] = useState([{value:'Damaged' , label: 'Damaged'} ,  {value:'Faulty' , label: 'Faulty'} , {value:'Used' , label: 'Used'}])
+  const {lists, onReturnDevice, onPhotos, onReturnStock} = props;
+  const [reason, setReason] = useState('');
+  const [reasonLists, setReasonLists] = useState([
+    {value: 'Damaged', label: 'Damaged'},
+    {value: 'Device Cleanup', label: 'Device Cleanup'},
+    {value: 'Faulty', label: 'Faulty'},
+    {value: 'Used', label: 'Used'},
+  ]);
   const [photos, setPhotos] = useState([]);
-  const [device, setDevice] = useState(null)
+  const [device, setDevice] = useState(null);
 
   return (
     <ScrollView style={styles.container}>
-                  
-          
-          <AppText title="Return Device" type="secondaryBold" size="medium"></AppText>
-            <DropdownInput
-              title="Select Device"            
-              lists={lists}
-              onItemSelected={(item) => {                
-                onReturnDevice({location_device_id: item.location_device_id, return_reason: reason});
-                setDevice(item);
+      <AppText
+        title="Return Device"
+        type="secondaryBold"
+        size="medium"></AppText>
+      <DropdownInput
+        title="Select Device"
+        lists={lists}
+        onItemSelected={item => {
+          onReturnDevice({
+            location_device_id: item.location_device_id,
+            return_reason: reason,
+          });
+          setDevice(item);
+        }}></DropdownInput>
 
-              }}
-            >
-          </DropdownInput>
+      <CSingleSelectInput
+        description={'Reason'}
+        placeholder={'Select ' + 'Reason'}
+        mode="single"
+        checkedValue={reason}
+        items={reasonLists}
+        hasError={false}
+        disabled={false}
+        onSelectItem={item => {
+          setReason(item.label);
+          if (device != null) {
+            onReturnDevice({
+              location_device_id: device.location_device_id,
+              return_reason: item.label,
+            });
+          }
+        }}
+        containerStyle={{marginTop: 15}}
+      />
 
-          <CSingleSelectInput                    
-              description={'Reason'}
-              placeholder={'Select ' + "Reason"}
-              mode='single'
-              checkedValue={reason}
-              items={reasonLists}
-              hasError={false}
-              disabled={false}
-              onSelectItem={item => {                    
-                  setReason(item.label);
-                  if(device != null){                    
-                    onReturnDevice({location_device_id: device.location_device_id, return_reason: item.label});
-                  }                                             
-              }}
-              containerStyle={{marginTop: 15}}
-          /> 
-          
-          <TakePhotoView
-              isOptimize={true}
-              onUpdatePhotos={photos => {                
-                setPhotos(photos);
-                onPhotos(photos);
-              }}
-              disabled={false}
-              photos={photos}
-              style={{marginVertical: 24}}
-          />              
+      <TakePhotoView
+        isOptimize={true}
+        onUpdatePhotos={photos => {
+          setPhotos(photos);
+          onPhotos(photos);
+        }}
+        disabled={false}
+        photos={photos}
+        style={{marginVertical: 24}}
+      />
 
-          <SubmitButton title={Strings.Stock.Return_Stock} style={{marginTop:10 , marginBottom:30}} onSubmit={onReturnStock}></SubmitButton>          
+      <SubmitButton
+        title={Strings.Stock.Return_Stock}
+        style={{marginTop: 10, marginBottom: 30}}
+        onSubmit={onReturnStock}></SubmitButton>
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-      alignSelf: 'stretch',
-      paddingTop: 10,
-      marginHorizontal: 20,      
-      paddingBottom:0,
-      //height:Dimensions.get("window").height * 0.6
+    alignSelf: 'stretch',
+    paddingTop: 10,
+    marginHorizontal: 20,
+    paddingBottom: 0,
+    //height:Dimensions.get("window").height * 0.6
   },
 
   imageContainer: {
@@ -82,6 +93,4 @@ const styles = StyleSheet.create({
     width: Dimensions.get('screen').width / 4.5 + 7,
     height: Dimensions.get('screen').width / 4.5,
   },
-
-})
-
+});
