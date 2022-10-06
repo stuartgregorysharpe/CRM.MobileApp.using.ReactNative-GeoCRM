2281cf6d912151171930e320080dec7bac81f6a7
Dev <redflame1028@gmail.com>
2022-10-07 00:01:57 +0800

    GV-204: customer sales history done

---
+++ screens/GeoRep/CRM/customer_sales/components/CustomerSalesHistoryView.js
@@ -0,0 +1,41 @@
+import {View, Text, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {AppText} from '../../../../../components/common/AppText';
+import {getApiRequest} from '../../../../../actions/api.action';
+import Colors, {whiteLabel} from '../../../../../constants/Colors';
+import TotalTurnoverView from './TotalTurnoverView';
+import SectionView from './SectionView';
+
+const CustomerSalesHistoryView = props => {
+  const {locationId, totalTurnOver, sections} = props;
+  const renderItems = (item, index) => {
+    return (
+      <SectionView item={item} key={index} style={{marginHorizontal: 10}} />
+    );
+  };
+  return (
+    <View style={styles.container}>
+      <FlatList
+        ListHeaderComponent={() => (
+          <TotalTurnoverView
+            data={totalTurnOver}
+            style={{marginTop: 16, marginHorizontal: 12}}
+          />
+        )}
+        data={sections}
+        renderItem={({item, index}) => renderItems(item, index)}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+});
+
+export default CustomerSalesHistoryView;
--- screens/GeoRep/CRM/customer_sales/containers/CustomerSalesHistoryContainer.js
+++ screens/GeoRep/CRM/customer_sales/containers/CustomerSalesHistoryContainer.js
@@ -1,15 +1,39 @@
-
-import { View } from 'react-native'
-import React from 'react'
-import CustomerSalesHistory from '../components/CustomerSalesHistory';
+import {ScrollView, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import CustomerSalesHistoryView from '../components/CustomerSalesHistoryView';
+import {Values} from '../../../../../constants';
+import {getApiRequest} from '../../../../../actions/api.action';
 
 export default function CustomerSalesHistoryContainer(props) {
-    
-    return (
-        <View style={{alignSelf:'stretch' , flex:1 , marginBottom:30}}>
-            <CustomerSalesHistory                            
-                {...props}
-            />
-        </View>
-    )
+  const {locationId} = props;
+  const [sections, setSections] = useState([]);
+  const [totalTurnOver, setTotalTurnOver] = useState(null);
+  useEffect(() => {
+    onLoad();
+  }, []);
+  const onLoad = () => {
+    const postData = {
+      location_id: locationId,
+    };
+    getApiRequest('locations/customer-sales-history-v2', postData)
+      .then(res => {
+        setSections(res.sections);
+        setTotalTurnOver(res.total_turnover);
+      })
+      .catch(e => {
+        console.log('E', e);
+      });
+  };
+  return (
+    <View
+      style={[{alignSelf: 'stretch', flex: 1, marginBottom: 30}, props.style]}>
+      <ScrollView style={{maxHeight: Values.deviceHeight * 0.8}}>
+        <CustomerSalesHistoryView
+          sections={sections}
+          totalTurnOver={totalTurnOver}
+          {...props}
+        />
+      </ScrollView>
+    </View>
+  );
 }
