a5ca16a6bed894e03ea27949e54423a22eb54af1
Dev <redflame1028@gmail.com>
2022-10-19 22:30:56 +0800

    GV-282: update function component to arrow function

--- screens/GeoRep/Stock/stock/modal/device/StockDeviceDetailsModal.js
+++ screens/GeoRep/Stock/stock/modal/device/StockDeviceDetailsModal.js
@@ -1,47 +1,48 @@
-
-import React from 'react'
+import React from 'react';
 import CModal from '../../../../../../components/common/CModal';
-import { Constants } from '../../../../../../constants';
+import {Constants} from '../../../../../../constants';
 import StockDetailsContainer from '../../container/StockDetailsContainer';
 
 const StockDeviceDetailsModal = React.forwardRef((props, ref) => {
-
-    const onButtonAction = data => {
-        if (props.onButtonAction) {
-          props.onButtonAction(data);
-        }
-        if (ref) {
-          ref.current.hideModal();
-        }
-    };
-    
-    const openSignature = (value) => {        
-        onButtonAction({ type: Constants.actionType.ACTION_NEXT , value: value });
+  const onButtonAction = data => {
+    if (ref) {
+      ref.current.hideModal();
     }
-    const openSwopAtTrader = (value) => {  
-        props.onButtonAction({ type: Constants.actionType.ACTION_NEXT , value: value });
+    if (props.onButtonAction) {
+      props.onButtonAction(data);
     }
-    const openTrader = (value) => {
-        onButtonAction({ type: Constants.actionType.ACTION_NEXT , value: value });
-    }
+  };
 
-    return (
-        <CModal
-            ref={ref}            
-            modalType={Constants.modalType.MODAL_TYPE_BOTTOM}
-            closableWithOutsideTouch
-            onClear={() => {
-                onButtonAction({ type: Constants.actionType.ACTION_FORM_CLEAR });
-            }}
-            {...props}>
-            <StockDetailsContainer
-                openSignature={openSignature}            
-                openSwopAtTrader={openSwopAtTrader}
-                openTrader={openTrader}
-                {...props} />
-        </CModal>        
-    )
+  const openSignature = value => {
+    onButtonAction({type: Constants.actionType.ACTION_NEXT, value: value});
+  };
+  const openSwopAtTrader = value => {
+    props.onButtonAction({
+      type: Constants.actionType.ACTION_NEXT,
+      value: value,
+    });
+  };
+  const openTrader = value => {
+    onButtonAction({type: Constants.actionType.ACTION_NEXT, value: value});
+  };
 
+  return (
+    <CModal
+      ref={ref}
+      modalType={Constants.modalType.MODAL_TYPE_BOTTOM}
+      closableWithOutsideTouch
+      onClear={() => {
+        onButtonAction({type: Constants.actionType.ACTION_FORM_CLEAR});
+      }}
+      {...props}>
+      <StockDetailsContainer
+        openSignature={openSignature}
+        openSwopAtTrader={openSwopAtTrader}
+        openTrader={openTrader}
+        {...props}
+      />
+    </CModal>
+  );
 });
 
 export default StockDeviceDetailsModal;
