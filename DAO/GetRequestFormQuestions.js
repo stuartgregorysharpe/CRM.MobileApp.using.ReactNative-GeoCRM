08830156c6f55a4d425b2e1dcaf2718cc2cdb6c5
Stuart <codechief19900@gmail.com>
2022-11-21 23:30:08 +0800

    Resolved Form question page issue in offline mode

--- DAO/GetRequestFormQuestions.js
+++ DAO/GetRequestFormQuestions.js
@@ -31,6 +31,7 @@ export function find(postData) {
             business_unit_id,
             postData,
           );
+          console.log("questionsLists",questionsLists)
           resolve({status: Strings.Success, questions: questionsLists});
         }
       })
@@ -328,192 +329,198 @@ const getFormQuestions = async (
   business_unit_id,
   postData,
 ) => {
+
   var tmp = [];
-  for (var i = 0; i < lists.length; i++) {
-    var element = lists.item(i);
-    const question_tag = element.question_tag;
-    var fieldData = '';
-    if (postData.location_id != undefined) {
-      if (question_tag != undefined && question_tag != '') {
-        if(question_tag === "msisdn"){
-          var primaryDeivce = await fetchPrimaryDeviceFromDB(postData.location_id);
-          fieldData = await getPrimaryDeviceData( primaryDeivce );                 
-        }else{
-          var fieldDataLists = await fetchFieldDetailsFromDB(
-            client_id,
-            business_unit_id,
-            question_tag,
-          );
-          fieldData = await getFieldData(fieldDataLists, postData);
+  try{
+    for (var i = 0; i < lists.length; i++) {
+
+      var element = lists.item(i);
+     
+      const question_tag = element.question_tag;
+      var fieldData = '';
+      if (postData.location_id != undefined) {
+        if (question_tag != undefined && question_tag != '') {
+          if(question_tag === "msisdn"){
+            var primaryDeivce = await fetchPrimaryDeviceFromDB(postData.location_id);
+            fieldData = await getPrimaryDeviceData( primaryDeivce );
+          }else{
+            var fieldDataLists = await fetchFieldDetailsFromDB(
+              client_id,
+              business_unit_id,
+              question_tag,
+            );
+            fieldData = await getFieldData(fieldDataLists, postData);
+          }          
         }
-        
       }
-    }
 
-    // Guide Info
-    var guideInfoData = [];
-    if (
-      element.guide_info_title != '' ||
-      element.guide_info_image != null ||
-      element.guide_info_text
-    ) {
-      var guideImagePath = '';
-      if (element.guide_info_image != '') {
-        guideImagePath =
-          baseURL +
-          `/common/assets/guide_info_images/` +
-          element.guide_info_image;
-      }
-      guideInfoData['title'] = element.guide_info_title;
-      guideInfoData['image'] = guideImagePath;
-      guideInfoData['text'] = element.guide_info_text;
-    }
-    // Get Options
-    var optionLists = await fetchOptionsFromDB(element.form_question_id);
-    var optionsData = getOptionData(optionLists);
-
-    // Trigger Data
-    var trigger = [];
-    if (element.trigger_question_type != '') {
-      var triggerOptions = [];
-      var triggerFieldType = '';
-      var triggerAnswer = '';
+      // Guide Info
+      var guideInfoData = [];
       if (
-        element.trigger_question_type == 'multiple' ||
-        element.trigger_question_type == 'multi_select'
+        element.guide_info_title != '' ||
+        element.guide_info_image != null ||
+        element.guide_info_text
       ) {
-        triggerFieldType = 'dropdown';
-        triggerAnswer = element.trigger_question_answer.split(',');
-      } else if (element.trigger_question_type == 'numbers') {
-        triggerFieldType = 'numbers';
-        triggerAnswer = element.trigger_question_answer;
-      } else {
-        triggerFieldType = 'text';
-        triggerAnswer = element.trigger_question_answer;
+        var guideImagePath = '';
+        if (element.guide_info_image != '') {
+          guideImagePath =
+            baseURL +
+            `/common/assets/guide_info_images/` +
+            element.guide_info_image;
+        }
+        guideInfoData['title'] = element.guide_info_title;
+        guideInfoData['image'] = guideImagePath;
+        guideInfoData['text'] = element.guide_info_text;
       }
-      trigger = {
-        question_id: element.trigger_question_id,
-        type: triggerFieldType,
-        condition: element.trigger_question_condition,
-        answer: triggerAnswer,
+      // Get Options
+      var optionLists = await fetchOptionsFromDB(element.form_question_id);
+      var optionsData = getOptionData(optionLists);
+  
+      // Trigger Data
+      var trigger = [];
+      if (element.trigger_question_type != '') {
+        var triggerOptions = [];
+        var triggerFieldType = '';
+        var triggerAnswer = '';
+        if (
+          element.trigger_question_type == 'multiple' ||
+          element.trigger_question_type == 'multi_select'
+        ) {
+          triggerFieldType = 'dropdown';
+          triggerAnswer = element.trigger_question_answer.split(',');
+        } else if (element.trigger_question_type == 'numbers') {
+          triggerFieldType = 'numbers';
+          triggerAnswer = element.trigger_question_answer;
+        } else {
+          triggerFieldType = 'text';
+          triggerAnswer = element.trigger_question_answer;
+        }
+        trigger = {
+          question_id: element.trigger_question_id,
+          type: triggerFieldType,
+          condition: element.trigger_question_condition,
+          answer: triggerAnswer,
+        };
+      }
+  
+      var bodyRes = {
+        form_question_id: element.form_question_id,
+        question_group: element.question_group,
+        question_group_id: element.question_group_id,
+        question_type: element.question_type,
+        question_text: element.question_text,
+        guide_info: guideInfoData,
+        rule_compulsory: element.rule_compulsory.toString(),
+        trigger: trigger,
+        value: fieldData,
       };
-    }
-
-    var bodyRes = {
-      form_question_id: element.form_question_id,
-      question_group: element.question_group,
-      question_group_id: element.question_group_id,
-      question_type: element.question_type,
-      question_text: element.question_text,
-      guide_info: guideInfoData,
-      rule_compulsory: element.rule_compulsory.toString(),
-      trigger: trigger,
-      value: fieldData,
-    };
-
-    const questionType = element.question_type;
-    if (questionType == 'yes_no' || questionType == 'take_photo') {
-      var include_image = [];
-      if (element.include_image != '') {
-        include_image = element.include_image.split(',');
-      }
-      var optimize = element.optimize_image;
-      tmp.push({
-        ...bodyRes,
-        include_image: include_image,
-        optimize: '',
-        rule_characters: element.rule_characters,
-        rule_editable: element.rule_editable.toString(),
-        add_prefix: element.add_prefix,
-        add_suffix: element.add_suffix,
-        question_tag: element.question_tag,
-        options: optionsData,
-      });
-    } else if (questionType == 'email_pdf') {
-      tmp.push({...bodyRes});
-    } else if (questionType == 'products') {
-      var productLists = await fetchProductsFromDB(business_unit_id, client_id);
-      var productsResults = getProductsData(productLists);
-
-      if (productsResults.length == 3) {
+  
+      const questionType = element.question_type;
+      if (questionType == 'yes_no' || questionType == 'take_photo') {
+        var include_image = [];
+        if (element.include_image != '') {
+          include_image = element.include_image.split(',');
+        }
+        var optimize = element.optimize_image;
         tmp.push({
           ...bodyRes,
-          product_types: productsResults[1],
-          brands: productsResults[2],
-          products: productsResults[0],
+          include_image: include_image,
+          optimize: '',
+          rule_characters: element.rule_characters,
+          rule_editable: element.rule_editable.toString(),
+          add_prefix: element.add_prefix,
+          add_suffix: element.add_suffix,
+          question_tag: element.question_tag,
+          options: optionsData,
         });
-      }
-    } else if (questionType == 'returns') {
-      const returnProductLists = await fetchReturnProductsFromDB(
-        business_unit_id,
-        client_id,
-      );
-      const returnProductsResults = getProductsData(returnProductLists);
-      const reasonList = await fetchReasonsFromDB(
-        business_unit_id,
-        client_id,
-        element.form_question_id,
-      );
-      const reasons = getReasonData(reasonList);
-      if (returnProductsResults.length == 3) {
+      } else if (questionType == 'email_pdf') {
+        tmp.push({...bodyRes});
+      } else if (questionType == 'products') {
+        var productLists = await fetchProductsFromDB(business_unit_id, client_id);
+        var productsResults = getProductsData(productLists);
+  
+        if (productsResults.length == 3) {
+          tmp.push({
+            ...bodyRes,
+            product_types: productsResults[1],
+            brands: productsResults[2],
+            products: productsResults[0],
+          });
+        }
+      } else if (questionType == 'returns') {
+        const returnProductLists = await fetchReturnProductsFromDB(
+          business_unit_id,
+          client_id,
+        );
+        const returnProductsResults = getProductsData(returnProductLists);
+        const reasonList = await fetchReasonsFromDB(
+          business_unit_id,
+          client_id,
+          element.form_question_id,
+        );
+        const reasons = getReasonData(reasonList);
+        if (returnProductsResults.length == 3) {
+          tmp.push({
+            ...bodyRes,
+            product_types: returnProductsResults[1],
+            brands: returnProductsResults[2],
+            products: returnProductsResults[0],
+            return_reasons: reasons,
+          });
+        }
+      } else if (questionType == 'pos_capture') {
+        /*const pleacementAreas = {};
+        const touchpoints = await fetchTouchpointsFromDB(
+          business_unit_id,
+          client_id,
+          element.form_question_id,
+        );
+        const placementAreaResult = await fetchPlacementAreasFromDB(
+          business_unit_id,
+          client_id,
+          element.form_question_id,
+        );
+  
+        placementAreaResult.forEach(result => {
+          pleacementAreas[result[placement_type]] = result[area];
+        });
+        const productsResult = await fetchPOSProductsFromDB(
+          business_unit_id,
+          client_id,
+        );
         tmp.push({
           ...bodyRes,
-          product_types: returnProductsResults[1],
-          brands: returnProductsResults[2],
-          products: returnProductsResults[0],
-          return_reasons: reasons,
+          touchpoints: touchpoints,
+          placement_areas: pleacementAreas,
+          products: productsResult,
+        });*/
+      } else if (
+        questionType == 'sku_select' ||
+        questionType == 'sku_count' ||
+        questionType == 'sku_shelf_share' ||
+        questionType == 'product_issues' ||
+        questionType == 'format_price' ||
+        questionType == 'brand_competitor_facings' ||
+        questionType == 'fsu_campaign' ||
+        questionType == 'tiered_multiple_choice'
+      ) {
+      } else {
+        tmp.push({
+          ...bodyRes,
+          include_image: [],
+          optimize: '0',
+          rule_characters: element.rule_characters,
+          rule_editable: element.rule_editable.toString(),
+          add_prefix: element.add_prefix,
+          add_suffix: element.add_suffix,
+          question_tag: element.question_tag,
+          options: optionsData,
         });
-      }
-    } else if (questionType == 'pos_capture') {
-      /*const pleacementAreas = {};
-      const touchpoints = await fetchTouchpointsFromDB(
-        business_unit_id,
-        client_id,
-        element.form_question_id,
-      );
-      const placementAreaResult = await fetchPlacementAreasFromDB(
-        business_unit_id,
-        client_id,
-        element.form_question_id,
-      );
-
-      placementAreaResult.forEach(result => {
-        pleacementAreas[result[placement_type]] = result[area];
-      });
-      const productsResult = await fetchPOSProductsFromDB(
-        business_unit_id,
-        client_id,
-      );
-      tmp.push({
-        ...bodyRes,
-        touchpoints: touchpoints,
-        placement_areas: pleacementAreas,
-        products: productsResult,
-      });*/
-    } else if (
-      questionType == 'sku_select' ||
-      questionType == 'sku_count' ||
-      questionType == 'sku_shelf_share' ||
-      questionType == 'product_issues' ||
-      questionType == 'format_price' ||
-      questionType == 'brand_competitor_facings' ||
-      questionType == 'fsu_campaign' ||
-      questionType == 'tiered_multiple_choice'
-    ) {
-    } else {
-      tmp.push({
-        ...bodyRes,
-        include_image: [],
-        optimize: '0',
-        rule_characters: element.rule_characters,
-        rule_editable: element.rule_editable.toString(),
-        add_prefix: element.add_prefix,
-        add_suffix: element.add_suffix,
-        question_tag: element.question_tag,
-        options: optionsData,
-      });
-    }
-  }
+      }    
+    }    
+  }catch(e){
+    console.log("form question error : ", e)
+  } 
   return tmp;
 };
 
@@ -611,6 +618,14 @@ const getProductsData = lists => {
   }
   return [productRes, product_types, brands];
 };
+
+const getPrimaryDeviceData = async(lists) => {
+  for (var i = 0; i < lists.length; i++) {
+      var element = lists.item(i);      
+      return element.device_msisdn;
+  }
+  return '';
+}
 
 export default {
   find,
