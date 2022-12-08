ddce02b1b47c601ee48dcbdb7b2e1a9609479f44
Dev <redflame1028@gmail.com>
2022-12-09 03:04:43 +0800

    GV-342: bugfix on cart screen

---
+++ screens/GeoRep/Sales/CartScreen.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Platform} from 'react-native';
+import BackButtonHeader from '../../../components/Header/BackButtonHeader';
+import {Colors} from '../../../constants';
+import CartContainer from './containers/CartContainer';
+
+const CartScreen = props => {
+  useEffect(() => {
+    refreshHeader();
+  });
+
+  const canGoBack = props.screenProps?.canGoBack();
+  const refreshHeader = () => {
+    if (props.screenProps) {
+      props.screenProps.setOptions({
+        headerTitle: () => {
+          return (
+            <BackButtonHeader title={'Cart'} navigation={props.navigation} />
+          );
+        },
+        tabBarStyle: {
+          height: 50,
+          paddingBottom: Platform.OS == 'android' ? 5 : 0,
+          backgroundColor: Colors.whiteColor,
+        },
+      });
+    }
+  };
+
+  return <CartContainer {...props} />;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'stretch',
+    flex: 1,
+  },
+});
+
+export default CartScreen;
