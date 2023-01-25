cbdcd4c7e98efe0d9e7b9827f837ffdaefa03cc4
Dev <redflame1028@gmail.com>
2023-01-25 15:23:42 +0800

    Revert "bugfix on CTextInput for ui layout"
    This reverts commit c3a4f5ff65548ccdfcb727a9dbeca07b52b25e6d.

--- components/common/CTextInput.js
+++ components/common/CTextInput.js
@@ -1,79 +1,70 @@
 import React, {useState, useEffect} from 'react';
-import {View, StyleSheet, Text, Platform, TextInput} from 'react-native';
+import {View, StyleSheet, Text, Platform , TextInput} from 'react-native';
 //import {TextInput} from 'react-native-paper';
 import {Colors, Fonts} from '../../constants';
 import {whiteLabel} from '../../constants/Colors';
 
-const CTextInput = props => {
-  const {multiline, cTextRef, dynamicFieldRef, index, add_prefix, add_suffix} =
-    props;
 
-  const hasError = props.hasError;
+const CTextInput = props => {
+
+  const { multiline, cTextRef, dynamicFieldRef , index , add_prefix, add_suffix} = props;  
+
+  const hasError = props.hasError;  
 
   const renderTopDescription = descriptionText => {
     return <Text style={styles.descriptionText}>{descriptionText}</Text>;
   };
 
   const getPrefixPadding = () => {
-    if (add_prefix != undefined && add_prefix !== '') {
+    if(add_prefix != undefined && add_prefix !== ''){
       return 8 * add_prefix.length;
     }
     return 0;
-  };
+  }
 
   return (
-    <View style={{alignSelf: 'stretch'}}>
-      <View
-        style={[
-          {
-            alignSelf: 'stretch',
-            borderColor: hasError
-              ? whiteLabel().endDayBackground
-              : whiteLabel().fieldBorder,
-          },
-          props.style,
-          styles.containerStyle,
-        ]}>
+    <View style={{flex:1}}>
+      <View style={[{
+          alignSelf: 'stretch' ,        
+          borderColor: hasError ? whiteLabel().endDayBackground : whiteLabel().fieldBorder }, 
+          props.style , styles.containerStyle]}>
+
         {add_prefix != undefined && add_prefix !== '' && (
-          <Text style={styles.prefixContainer}> {add_prefix} </Text>
+          <Text
+            style={styles.prefixContainer}>
+            {' '}
+            {add_prefix}{' '}
+          </Text>
         )}
 
-        {props.label && props.label != '' && renderTopDescription(props.label)}
+        {
+          props.label && props.label != '' &&
+          renderTopDescription(props.label)
+        }
 
-        <TextInput
+        <TextInput        
+          
           ref={element => {
-            if (
-              dynamicFieldRef != undefined &&
-              index != undefined &&
-              dynamicFieldRef.current != undefined
-            ) {
+            if(dynamicFieldRef != undefined && index != undefined &&  dynamicFieldRef.current != undefined){
               dynamicFieldRef.current[index] = element;
             }
-            if (cTextRef != undefined && cTextRef != null) {
+            if(cTextRef != undefined && cTextRef != null){
               cTextRef.current = element;
             }
-          }}
+          }}        
           disabled={props.disabled != undefined ? props.disabled : false}
           //mode="outlined"
-          multiline={multiline != undefined ? multiline : false}
+          multiline={multiline != undefined ? multiline : false}        
           //numberOfLines={3}
           autoGrow
           outlineColor={
-            props.hasError
-              ? whiteLabel().endDayBackground
-              : whiteLabel().fieldBorder
+            props.hasError ? whiteLabel().endDayBackground : whiteLabel().fieldBorder
           }
           activeOutlineColor={
-            props.hasError
-              ? whiteLabel().endDayBackground
-              : Colors.disabledColor
+            props.hasError ? whiteLabel().endDayBackground : Colors.disabledColor
           }
-          {...props}
-          style={[
-            multiline ? styles.multilineTextInput : styles.textInput,
-            props.textInputStyle,
-            {marginLeft: getPrefixPadding()},
-          ]}
+          {...props}        
+          style={[ multiline ? styles.multilineTextInput : styles.textInput, props.textInputStyle , {marginLeft : getPrefixPadding()} ]}                  
           onSubmitEditing={() => {
             if (
               dynamicFieldRef != undefined &&
@@ -81,94 +72,94 @@ const CTextInput = props => {
               index <= dynamicFieldRef.current.length - 2 &&
               dynamicFieldRef.current[index + 1] != null
             ) {
-              if (!multiline) {
+              if(!multiline){
                 dynamicFieldRef.current[index + 1].focus();
-              }
+              }            
             }
-            if (props.onSubmitEditing) {
+            if(props.onSubmitEditing){
               props.onSubmitEditing();
             }
           }}
         />
 
-        {props.hasError &&
-          props.isRequired &&
-          (add_suffix == undefined || add_suffix == '') && (
-            <View style={{position: 'absolute', right: 0, top: 15}}>
-              <Text
-                style={[
-                  {
-                    color: whiteLabel().endDayBackground,
-                    marginHorizontal: 10,
-                    fontFamily: Fonts.primaryRegular,
-                  },
-                  props.errorTextStyle,
-                ]}>
-                (required)
-              </Text>
-            </View>
-          )}
+
+        {props.hasError && props.isRequired && (add_suffix == undefined || add_suffix == '') && (
+          <View style={{position: 'absolute', right: 0, top: 15}}>
+            <Text
+              style={[
+                {
+                  color: whiteLabel().endDayBackground,
+                  marginHorizontal: 10,
+                  fontFamily: Fonts.primaryRegular,
+                },
+                props.errorTextStyle,
+              ]}>
+              (required)
+            </Text>
+          </View>
+        )}
 
         {add_suffix != undefined && add_suffix !== '' && (
-          <Text style={styles.suffixContainer}> {add_suffix} </Text>
+            <Text
+              style={styles.suffixContainer}>
+              {' '}
+              {add_suffix}{' '}
+            </Text>
         )}
+
+        
       </View>
-      {props.errorText && props.hasError && (
-        <Text
-          style={{
-            color: whiteLabel().endDayBackground,
-            fontSize: 12,
-            marginLeft: 5,
-            marginTop: 3,
-          }}>
-          {props.errorText}
-        </Text>
-      )}
+      {
+          props.errorText && props.hasError &&
+          <Text style={{color:whiteLabel().endDayBackground, fontSize:12, marginLeft:5, marginTop:3}} >{props.errorText}</Text>
+      }
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  containerStyle: {
-    borderRadius: 4,
-    borderWidth: 1,
-    padding: 3,
-    paddingTop: 5,
-    justifyContent: 'center',
+
+  containerStyle :{
+    borderRadius: 4,    
+    borderWidth:1,
+    padding:3,
+    paddingTop:5,
+    justifyContent:'center',    
   },
 
   textInput: {
-    alignSelf: 'stretch',
-    height: 35,
+    alignSelf:'stretch',    
+    height:35,
     fontSize: 14,
-    paddingTop: 0,
-    paddingBottom: 0,
+    paddingTop:0,
+    paddingBottom:0,
     backgroundColor: Colors.bgColor,
-    fontFamily: Fonts.secondaryMedium,
-    justifyContent: 'center',
+    fontFamily: Fonts.secondaryMedium,         
+    justifyContent:'center',  
   },
 
-  multilineTextInput: {
-    fontSize: 14,
+  multilineTextInput :{
+    fontSize:14,    
     backgroundColor: Colors.bgColor,
     fontFamily: Fonts.secondaryMedium,
-    maxHeight: Platform.OS == 'android' ? 125 : 130,
+    maxHeight:Platform.OS == 'android' ? 125 : 130
   },
-  prefixContainer: {
-    marginTop: 18,
+  prefixContainer: {    
+    marginTop:18,
     color: whiteLabel().helpText,
-    fontSize: 12,
-    position: 'absolute',
+    fontSize: 12,    
+    position:'absolute',    
     left: -1,
-    zIndex: 999,
+    zIndex: 999
   },
   suffixContainer: {
-    marginTop: 18,
+    marginTop:18,
     color: whiteLabel().helpText,
-    fontSize: 12,
-    position: 'absolute',
-    right: 0,
-    zIndex: 999,
+    fontSize: 12,    
+    position:'absolute',    
+    right:0,
+    zIndex: 999
   },
   descriptionText: {
     position: 'absolute',
@@ -178,7 +169,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 2,
     color: Colors.disabledColor,
     backgroundColor: Colors.bgColor,
-    zIndex: 999999999999999,
+    zIndex:999999999999999
   },
 });
 
