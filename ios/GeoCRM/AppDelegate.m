45c56b8c24f383c47e4e59d0b67886952d71a936
Dev <redflame1028@gmail.com>
2023-01-31 21:25:44 +0800

    notifications: setup ios firebase

--- ios/GeoCRM/AppDelegate.m
+++ ios/GeoCRM/AppDelegate.m
@@ -5,7 +5,7 @@
 #import <React/RCTRootView.h>
 #import "RNBootSplash.h"
 #import <GoogleMaps/GoogleMaps.h>
-
+#import <Firebase.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -30,6 +30,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
