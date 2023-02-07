fc2071696bf73b5ff2855ee3a088a19e952a513b
Stuart <codechief19900@gmail.com>
2023-02-08 03:20:04 +0800

    GV-CRM - Add device to stock blank notification

--- screens/GeoRep/Stock/stock/container/AddStockContainer.js
+++ screens/GeoRep/Stock/stock/container/AddStockContainer.js
@@ -1,18 +1,14 @@
 import {View} from 'react-native';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState , useRef } from 'react';
 import AddStockView from '../components/AddStockView';
 import {Constants, Strings} from '../../../../../constants';
 import {useSelector} from 'react-redux';
 import {expireToken, getPostParameter} from '../../../../../constants/Helper';
-import {
-  clearNotification,
-  showNotification,
-} from '../../../../../actions/notification.action';
 import {useDispatch} from 'react-redux';
-import {Notification} from '../../../../../components/modal/Notification';
 import {GetRequestStockFieldDataDAO, PostRequestDAO} from '../../../../../DAO';
 import { generateKey } from '../../../../../constants/Utils';
-import LoadingProgressBar from '../../../../../components/modal/LoadingProgressBar';
+import LoadingBar from '../../../../../components/LoadingView/loading_bar';
+import AlertDialog from '../../../../../components/modal/AlertDialog';
 
 var add_stock_indempotency = '';
 
@@ -20,10 +16,14 @@ export default function AddStockContainer(props) {
 
   const dispatch = useDispatch();
   const [deviceTypeLists, setDevicetypeLists] = useState([]);
-  const [stockTypes, setStockTypes] = useState({});
-  const {items} = props;
+  const [stockTypes, setStockTypes] = useState({});  
   const currentLocation = useSelector(state => state.rep.currentLocation);
   const [isLoading, setIsLoading] = useState(false);
+  const [isConfirmModal, setIsConfirmModal] = useState(false)
+  const [message, setMessage] = useState('');
+  const [messageType, setMessageType] = useState('success');
+  const loadingBarRef = useRef(null);
+
   let isMount = true;
 
   useEffect(() => {
@@ -68,6 +68,7 @@ export default function AddStockContainer(props) {
     } else {
       subTitle = data.sims.map(item => item.network).join(', ');
     }
+    loadingBarRef.current.showModal();
     PostRequestDAO.find(
       0,
       data,
@@ -75,49 +76,54 @@ export default function AddStockContainer(props) {
       'stockmodule/add-stock',
       type,
       subTitle,
-      add_stock_indempotency,
-      dispatch
+      add_stock_indempotency      
     )
       .then(res => {
         setIsLoading(false);
+        loadingBarRef.current.hideModal();
+        console.log("add stock response =>", res);
+        var message = '';
         if (res.status === Strings.Success) {
-          dispatch(
-            showNotification({
-              type: Strings.Success,
-              message: res.message,
-              buttonText: 'Ok',
-              buttonAction: async () => {
-                props.onButtonAction({type: Constants.actionType.ACTION_CLOSE});
-                dispatch(clearNotification());
-              },
-            }),
-          );
-        } else {
-          dispatch(
-            showNotification({
-              type: Strings.Success,
-              message: res.errors,
-              buttonText: 'Ok',
-            }),
-          );
+          message = res.message != null ? res.message : 'Exist Same IMEI';
+        }else{
+          message = res.errors;
         }
+        setMessageType(res.status);
+        setIsConfirmModal(true);
+        setMessage(message);
       })
       .catch(e => {
         setIsLoading(false);
+        loadingBarRef.current.hideModal();
         expireToken(dispatch, e);
       });
   };
 
   return (
     <View style={{alignSelf: 'stretch'}}>
+
       <AddStockView
         callAddStock={callAddStock}
         stockTypes={stockTypes}
         deviceTypeLists={deviceTypeLists}        
         {...props}
       />
-      <Notification />
-      <LoadingProgressBar />
+
+      <LoadingBar 
+        backButtonDisabled={true}
+        ref={loadingBarRef} />
+
+      <AlertDialog
+        visible={isConfirmModal}
+        onModalClose={() => {
+          setIsConfirmModal(false);     
+          if(messageType == Strings.Success){
+            props.onButtonAction({type: Constants.actionType.ACTION_CLOSE});
+          }
+        }}
+        message={message}>        
+        </AlertDialog>      
+
     </View>
   );
 }
