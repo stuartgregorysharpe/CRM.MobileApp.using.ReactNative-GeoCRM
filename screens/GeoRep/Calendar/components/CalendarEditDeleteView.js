84f9da2cf6bedd7e7214e3dff3c49bd37451a742
Stuart <codechief19900@gmail.com>
2023-02-21 16:51:45 +0800

    GV-917 -> file template

---
+++ screens/GeoRep/Calendar/components/CalendarEditDeleteView.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CalendarEditDeleteView = () => {
+  return (
+    <View>
+      <Text>CalendarEditDeleteView</Text>
+    </View>
+  )
+}
+
+export default CalendarEditDeleteView
---
+++ screens/GeoRep/Calendar/containers/CalendarEditDeleteModalContainer.js
@@ -0,0 +1,16 @@
+
+import { View, Text } from 'react-native'
+import React from 'react'
+import CalendarEditDeleteView from '../components/CalendarEditDeleteView'
+
+const CalendarEditDeleteModalContainer = () => {
+
+  return (
+    <View>
+        <CalendarEditDeleteView />
+    </View>
+  )
+   
+}
+
+export default CalendarEditDeleteModalContainer
