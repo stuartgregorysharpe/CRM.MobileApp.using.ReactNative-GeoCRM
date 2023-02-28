e227894d6a7faf4a4e68159bf1bb68daff68cf25
Stuart <codechief19900@gmail.com>
2023-02-28 22:03:39 +0800

    GV-938 -> Geo CRM - Sim card report - Carousel is looping

--- screens/GeoRep/CRM/sim_card/components/SimCardReportView.js
+++ screens/GeoRep/CRM/sim_card/components/SimCardReportView.js
@@ -1,32 +1,14 @@
-import { Dimensions, StyleSheet, Text, View , Platform } from 'react-native'
+import { Dimensions, StyleSheet, View , ScrollView } from 'react-native'
 import React , { useState } from 'react'
-import Carousel from 'react-native-reanimated-carousel';
-import {
-	BaseButton,
-	GestureHandlerRootView,
-	ScrollView
-  } from 'react-native-gesture-handler';
 import NetworkItem from './NetworkItem';
 import TableView from './TableView';
 import SvgIcon from '../../../../../components/SvgIcon';
 
-const PAGE_WIDTH = Dimensions.get("screen").width;
-const COUNT = 3;
-
 const SimCardReportView = (props) => {
 
   	const { monthLabels , data , networks } = props;
-
 	const [selectedIndex, setSelectedIndex] = useState(0);
 
-  	const baseOptions = {
-		vertical: false,
-		width: ( PAGE_WIDTH - 40) / COUNT,
-		height: 50,
-		style: {
-			width: PAGE_WIDTH - 40,
-		},
-	};
 
 	return (
 		<View>
@@ -35,9 +17,7 @@ const SimCardReportView = (props) => {
 					showsHorizontalScrollIndicator={false}
 					contentContainerStyle={{
 						marginTop:12,
-						flexGrow: 1,
-						//justifyContent: 'center',
-						//width: Dimensions.get("screen").width - 40,
+						flexGrow: 1,						
 					}}
 					horizontal={true} >
 
@@ -53,26 +33,7 @@ const SimCardReportView = (props) => {
 							)
 						})
 					}
-
 				</ScrollView>
-
-				{/* <GestureHandlerRootView>      
-					<Carousel
-						{ ...baseOptions }
-						loop={false}			
-						data={networks}					
-						onSnapToItem={(index) => {						
-						}}
-						renderItem={({ index }) => (			
-							<NetworkItem 
-								key={index}
-								onItemSelected={(index) => {
-									setSelectedIndex(index);
-								}}
-								item={networks[index]} index={index} selectedIndex={selectedIndex} />						
-						)}
-					/>						
-				</GestureHandlerRootView> */}
 				<SvgIcon icon="Angle_Left_form" width='15' height='15' style={{marginLeft:5, marginRight:5}} />
 			</View>
 
