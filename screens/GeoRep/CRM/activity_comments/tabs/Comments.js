293a91e371a47fb293a7729e1498bee2c349ec80
Stuart <codechief19900@gmail.com>
2023-05-25 21:28:59 +0800

    GV-921 -> Geo CRM - 'Network error' when resubmitting form after taking photo

--- screens/GeoRep/CRM/activity_comments/tabs/Comments.js
+++ screens/GeoRep/CRM/activity_comments/tabs/Comments.js
@@ -4,6 +4,7 @@ import {
   StyleSheet,  
   ActivityIndicator,  
   FlatList,
+  Platform,
 } from 'react-native';
 import {useDispatch} from 'react-redux';
 import {AppText} from '../../../../../components/common/AppText';
@@ -16,6 +17,8 @@ import FormQuestionModal from '../../add_lead/modal/FormQuestionModal';
 import { clearNotification, showNotification } from '../../../../../actions/notification.action';
 import { getFormSubmissionPostJsonData } from '../../../Forms/questions/helper';
 import { useSelector } from 'react-redux';
+import AlertModal from '../../../../../components/modal/AlertModal';
+import LoadingBar from '../../../../../components/LoadingView/loading_bar';
 
 export default function Comments(props) {
   
@@ -25,6 +28,8 @@ export default function Comments(props) {
 	const [page, setPage] = useState(0);
 	const [locationName , setLocationName] = useState("");
 	const formQuestionModalRef = useRef(null);
+	const alertModalRef = useRef();
+	const loadingBarRef = useRef();
 	const [form, setForm] = useState({});
 
 	const currentLocation = useSelector(state => state.rep.currentLocation);
@@ -32,7 +37,7 @@ export default function Comments(props) {
 	let isMount = true;
 
 	useEffect(() => {
-
+		isMount = true;
 		getFormSubmissions(page);
 		return () => {
 			isMount = false;
@@ -73,20 +78,17 @@ export default function Comments(props) {
 	}
 
 	const editFormQuestion = async (form_answers, files) => {			
-		
+		showLoadingBar();
 		const postDataJson = await getFormSubmissionPostJsonData(form.submission_id, location_id , currentLocation, form_answers, files , "edit" );									
 		PostRequestDAO.find(location_id, postDataJson , 'form_submission', 'forms/forms-submission' 
-		, form.form_name , '' , null , dispatch ).then( async(res) => {									
-			if(res.status === Strings.Success){
-				dispatch(showNotification({type: Strings.Success , message: res.message , buttonText:'Ok', buttonAction:() => {					
-					setPage(0)
-					getFormSubmissions(0);					
-					dispatch(clearNotification());
-				}}));
+		, form.form_name , '' , null , null ).then( async(res) => {									
+			hideLoadingBar();
+			if(res.status === Strings.Success){								
+				showMessage(res.message, 'Ok');
 			}
 		}).catch((e) => {
-			console.log(e);
-			expireToken(dispatch, e);
+			hideLoadingBar();
+			expireToken(dispatch, e , alertModalRef);
 		})
 	}
 
@@ -101,6 +103,27 @@ export default function Comments(props) {
 			}
 		}
 	};
+
+	const showLoadingBar = () => {
+		if(loadingBarRef.current){
+			loadingBarRef.current.showModal();
+		}
+	}
+	const hideLoadingBar = () => {
+		if(loadingBarRef.current){
+			loadingBarRef.current.hideModal();
+		}
+	}
+	const showMessage = (message) => {
+		const delay = Platform.OS == 'ios' ? 500 : 0;
+		setTimeout(() => {
+			if(alertModalRef.current){
+				alertModalRef.current.alert(message);
+			}
+		}, delay);
+		
+	}
+
 
 	const renderItems = (item, index) => {
 		return (
@@ -131,7 +154,16 @@ export default function Comments(props) {
 	);
 
   return (
-    <View style={styles.container}>      
+    <View style={styles.container}>
+
+		<AlertModal 
+			onModalClose={(res) => {
+				setPage(0);
+				getFormSubmissions(0);									
+			}}
+			ref={alertModalRef} /> 
+		<LoadingBar ref={loadingBarRef} />
+
 		<View style={{marginTop: 5, marginBottom: 10 , marginLeft:5}}>
 			<AppText size="medium" type="secondaryBold" title={Strings.CRM.Form_Activity_For + locationName}></AppText>
 		</View>
@@ -157,10 +189,7 @@ export default function Comments(props) {
 				hideClear={true}
 				title=""
 				form={form}
-				submissionType={"edit"}
-				//leadForms={leadForms}	
-				//customMasterFields={customMasterFields}
-				//selectedLists={selectedLists}
+				submissionType={"edit"}				
 				onButtonAction={onFormQuestionModalClosed}
 			/> 
 
