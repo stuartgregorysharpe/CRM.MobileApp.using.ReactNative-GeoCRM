d1e10e20acb0f1b2c8220caebc564a6f90106f2f
FullStack1106 <0supportx@gmail.com>
2023-06-02 00:11:34 +0200

    Complte ui design with static data

--- screens/GeoRep/Learning/containers/LearningContainer.js
+++ screens/GeoRep/Learning/containers/LearningContainer.js
@@ -2,74 +2,109 @@ import {View, Text, TouchableOpacity, Dimensions, ScrollView} from 'react-native
 import React, {useEffect, useState} from 'react';
 import LearningGradientView from '../components/LearningGradientView';
 import CourseListCardView from '../components/CourseListCardView';
+import { getApiRequest } from '../../../../actions/api.action';
 
 const LearningContainer = props => {
     const [courseList, setCourseList] = useState([]);
     const [userSummary, setUserSummary] = useState({});
     useEffect(() => {
-      setCourseList([
-        {
-          "course_id": "6546356546346",
-          "status": "in_progress",
-          "course_name": "Water & Electricity Sales",
-          "course_icon": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
-          "lessons": {
-              "completed": "1",
-              "total": "4"
-          },
-          "points": "200",
-          "next_item": {
-              "type": "lesson_step",
-              "id": "574887648748745"
-          }
-        },
-        {
-            "course_id": "9865345634636",
-            "status": "in_progress",
-            "course_name": "Course Title",
-            "course_icon": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
-            "lessons": {
-                "completed": "1",
-                "total": "5"
-            },
-            "points": "200",
-            "next_item": {
-                "type": "lesson_step",
-                "id": "3252354266"
-            }
-        },
-        {
-            "course_id": "986534525634636",
-            "status": "in_progress",
-            "course_name": "Long Course Name Example Course 225",
-            "course_icon": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
-            "lessons": {
-                "completed": "2",
-                "total": "7"
-            },
-            "points": "200",
-            "next_item": {
-                "type": "lesson_step",
-                "id": "37547476"
-            }
-        },
-        {
-            "course_id": "5234523452345",
-            "status": "completed",
-            "course_name": "POPIA Policy Course",
-            "course_icon": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
-            "lessons": {
-                "completed": "7",
-                "total": "7"
-            },
-            "points": "200",
-            "next_item": {
-                "type": "",
-                "id": ""
-            }
-        }
-      ])
+      getLearningDashboard()
     }, []);
+
+    const getLearningDashboard = () =>{
+        // getApiRequest('index.php/v2/user/dashboard', {}).then(res =>{
+            // console.log('Learning Dashboard Api result', res)
+            let res = {
+                "status": "success",
+                "user_summary": {
+                    "name": "Test User 1",
+                    "image": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
+                    "total_points": "200",
+                    "courses": {
+                        "completed": "2",
+                        "total": "6"
+                    }
+                },
+                "current_course": {
+                    "course_id": "6546356546346",
+                    "course_name": "Water & Electricity Sales",
+                    "course_icon": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
+                    "points": "200",
+                    "lessons": {
+                        "completed": "1",
+                        "total": "4"
+                    },
+                    "progress": "25"
+                },
+                "assigned_courses": [
+                    {
+                        "course_id": "6546356546346",
+                        "status": "in_progress",
+                        "course_name": "Water & Electricity Sales",
+                        "course_icon": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
+                        "lessons": {
+                            "completed": "1",
+                            "total": "4"
+                        },
+                        "points": "200",
+                        "next_item": {
+                            "type": "lesson_step",
+                            "id": "574887648748745"
+                        }
+                    },
+                    {
+                        "course_id": "9865345634636",
+                        "status": "in_progress",
+                        "course_name": "Course Title",
+                        "course_icon": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
+                        "lessons": {
+                            "completed": "1",
+                            "total": "5"
+                        },
+                        "points": "200",
+                        "next_item": {
+                            "type": "lesson_step",
+                            "id": "3252354266"
+                        }
+                    },
+                    {
+                        "course_id": "986534525634636",
+                        "status": "in_progress",
+                        "course_name": "Long Course Name Example Course 225",
+                        "course_icon": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
+                        "lessons": {
+                            "completed": "2",
+                            "total": "7"
+                        },
+                        "points": "200",
+                        "next_item": {
+                            "type": "lesson_step",
+                            "id": "37547476"
+                        }
+                    },
+                    {
+                        "course_id": "5234523452345",
+                        "status": "completed",
+                        "course_name": "POPIA Policy Course",
+                        "course_icon": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
+                        "lessons": {
+                            "completed": "7",
+                            "total": "7"
+                        },
+                        "points": "200",
+                        "next_item": {
+                            "type": "",
+                            "id": ""
+                        }
+                    }
+                ]
+            }
+
+            setCourseList(res.assigned_courses)
+            setUserSummary(res.user_summary)
+
+        // })
+    }
     return (
         <ScrollView
             style= {{
