f3c6c1c682a4579a222828be99a452a584850605
FullStack1106 <0supportx@gmail.com>
2023-06-02 09:15:04 +0200

    implement alert modal for api request exception and define functions to show/hide loading bar

--- screens/GeoRep/Learning/containers/LearningContainer.js
+++ screens/GeoRep/Learning/containers/LearningContainer.js
@@ -4,23 +4,35 @@ import LearningGradientView from '../components/LearningGradientView';
 import CourseListCardView from '../components/CourseListCardView';
 import { getApiRequest } from '../../../../actions/api.action';
 import LoadingBar from '../../../../components/LoadingView/loading_bar';
+import AlertModal from '../../../../components/modal/AlertModal';
+import { expireToken } from '../../../../constants/Helper';
 
 const LearningContainer = props => {
     const [courseList, setCourseList] = useState([]);
     const [userSummary, setUserSummary] = useState({});
     const loadingBarRef = useRef(null);
+    const alertModalRef = useRef(null);
     useEffect(() => {
       getLearningDashboard()
     }, []);
-
-    const getLearningDashboard = () =>{
+    const showLoadingBar = () => {
+        if(loadingBarRef.current)
         loadingBarRef.current.showModal();
+    }
+
+    const hideLoadingBar = () => {
+        if(loadingBarRef.current)
+        loadingBarRef.current.hideModal();
+    }
+    const getLearningDashboard = () =>{
+        showLoadingBar();
         getApiRequest('v2/user/dashboard', {}, true).then(response =>{
             setCourseList(response.assigned_courses)
             setUserSummary(response.user_summary)
-            loadingBarRef.current.hideModal();
+            hideLoadingBar();
         }).catch(error =>{
-            loadingBarRef.current.hideModal()
+            hideLoadingBar();
+            expireToken(dispatch, error , alertModalRef); 
         })
     }
     return (
@@ -36,6 +48,8 @@ const LearningContainer = props => {
             <CourseListCardView icon_name = "Verified" section_title = "Completed Courses" course_list = {courseList.filter(element => element.status == 'completed')}/>
             {/* Loading Bar */}
             <LoadingBar ref={loadingBarRef}/>
+            {/* Alert Dialog */}
+            <AlertModal ref = {alertModalRef}/>
         </ScrollView>
     );
 }
