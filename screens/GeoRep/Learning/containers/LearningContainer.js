f43309eec92f22c6a9b3bb2ca58de4770a5a3969
FullStack1106 <0supportx@gmail.com>
2023-06-02 09:11:24 +0200

    Remove static json and integrate with live response

--- screens/GeoRep/Learning/containers/LearningContainer.js
+++ screens/GeoRep/Learning/containers/LearningContainer.js
@@ -16,99 +16,10 @@ const LearningContainer = props => {
     const getLearningDashboard = () =>{
         loadingBarRef.current.showModal();
         getApiRequest('v2/user/dashboard', {}, true).then(response =>{
-            console.log('Learning Dashboard Api result', response)
-            let res = {
-                "status": "success",
-                "user_summary": {
-                    "name": "Test User 1",
-                    "image": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
-                    "total_points": "200",
-                    "courses": {
-                        "completed": "2",
-                        "total": "6"
-                    }
-                },
-                "current_course": {
-                    "course_id": "6546356546346",
-                    "course_name": "Water & Electricity Sales",
-                    "course_icon": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
-                    "points": "200",
-                    "lessons": {
-                        "completed": "1",
-                        "total": "4"
-                    },
-                    "progress": "25"
-                },
-                "assigned_courses": [
-                    {
-                        "course_id": "6546356546346",
-                        "status": "in_progress",
-                        "course_name": "Water & Electricity Sales",
-                        "course_icon": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
-                        "lessons": {
-                            "completed": "1",
-                            "total": "4"
-                        },
-                        "points": "200",
-                        "next_item": {
-                            "type": "lesson_step",
-                            "id": "574887648748745"
-                        }
-                    },
-                    {
-                        "course_id": "9865345634636",
-                        "status": "in_progress",
-                        "course_name": "Course Title",
-                        "course_icon": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
-                        "lessons": {
-                            "completed": "1",
-                            "total": "5"
-                        },
-                        "points": "200",
-                        "next_item": {
-                            "type": "lesson_step",
-                            "id": "3252354266"
-                        }
-                    },
-                    {
-                        "course_id": "986534525634636",
-                        "status": "in_progress",
-                        "course_name": "Long Course Name Example Course 225",
-                        "course_icon": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
-                        "lessons": {
-                            "completed": "2",
-                            "total": "7"
-                        },
-                        "points": "200",
-                        "next_item": {
-                            "type": "lesson_step",
-                            "id": "37547476"
-                        }
-                    },
-                    {
-                        "course_id": "5234523452345",
-                        "status": "completed",
-                        "course_name": "POPIA Policy Course",
-                        "course_icon": "https://dev.georep.com/common/assets/users/profile_pics/geo_rep_fav.png",
-                        "lessons": {
-                            "completed": "7",
-                            "total": "7"
-                        },
-                        "points": "200",
-                        "next_item": {
-                            "type": "",
-                            "id": ""
-                        }
-                    }
-                ]
-            }
-
-            setCourseList(res.assigned_courses)
-            setUserSummary(res.user_summary)
+            setCourseList(response.assigned_courses)
+            setUserSummary(response.user_summary)
             loadingBarRef.current.hideModal();
-
         }).catch(error =>{
-            console.log("API for Learning Dashboard Error:", error)
             loadingBarRef.current.hideModal()
         })
     }
